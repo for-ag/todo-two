2ad88f9a2fd98aed91efa57fa105aa7a261a1be4
Josh <145994834+for-ag@users.noreply.github.com>
2024-06-13 08:57:57 -0500

    added vars and selectors

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // Retrieve todo from local storage or init empty array
 
-let todo = JSON.parse();
+let todo = JSON.parse(localStorage.getItem('todo')) || [];
+const todoInput = document.getElementById('todoInput');
+const todoList = document.getElementById('todoList');
+const todoCount = document.getElementById('todoCount');
+const addButton = document.querySelector('.btn');
+const deleteButton = document.getElementById('deleteButton')
+
+// Init 
