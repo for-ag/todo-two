e9cad991919b618bd638e189972f2cc721aea53b
Josh <145994834+for-ag@users.noreply.github.com>
2024-06-13 18:13:58 -0500

    added todo task addition function

--- script.js
+++ script.js
@@ -8,3 +8,40 @@ const addButton = document.querySelector('.btn');
 const deleteButton = document.getElementById('deleteButton')
 
 // Init 
+
+document.addEventListener("DOMContentLoaded", function () {
+        addButton.addEventListener("click" , addTask);
+        todoInput.addEventListener('keydown', function (event) {
+            if (event.key === 'Enter') {
+                event.preventDefault();
+                addTask();
+            }
+        })
+        deleteButton.addEventListener('click', deleteAllTasks);
+        displayTasks();
+});
+
+function addTask() {
+    const newTask = todoInput.value.trim();
+    if (newTask !== "") {
+        todo.push({
+            text: newTask,
+            disabled: false,
+        });
+        saveToLocalStorage();
+        todoInput.value = "";
+        displayTasks();
+    }
+}
+
+function deleteAllTasks() {
+    console.log("test");
+}
+
+function displayTasks() {
+
+}
+
+function saveToLocalStorage () {
+    localStorage.setItem("todo", JSON.stringify(todo));
+}
